19322901c9daa710e0f1743e77206335d4d62a62
Sanjeevitha <sanjeevithabtech@gmail.com>
2025-12-18 10:08:15 +0000

    use this image link : "https://th.bing.com/th/id/OIP.ujwQ4yJGp4mXOsS8b3e

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'th.bing.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
